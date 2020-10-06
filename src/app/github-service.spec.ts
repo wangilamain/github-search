0c3ac3e8255b138be156bfc2d87102e58653e265
wangilamain <wangilayng@gmail.com>
2020-10-06 12:04:12 -0400

    added a github class

--- src/app/github-service.spec.ts
+++ src/app/github-service.spec.ts
@@ -1,7 +1,16 @@
-import { GithubService } from './github-service';
+import { TestBed } from '@angular/core/testing';
+
+import { GithubService } from './github.service';
 
 describe('GithubService', () => {
-  it('should create an instance', () => {
-    expect(new GithubService()).toBeTruthy();
+  let service: GithubService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GithubService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
--- src/app/github-service.ts
+++ src/app/github-service.ts
@@ -1,2 +1,23 @@
+import { environment } from './../environments/environment';
+import { repo } from './repo';
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
 export class GithubService {
+
+  baseURL: string = 'https://api.github.com';
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getUserRepos(username:string): Observable<repo[]> {
+    return this.http.get<repo[]>(this.baseURL + '/users/' + username + '/repos');
+    
+  }
 }
