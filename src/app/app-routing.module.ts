60ac2008076bf0e999cf33d58eb763fdcebfc743
wangilamain <wangilayng@gmail.com>
2020-10-06 11:55:36 -0400

    added repo content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { GhRepoComponent } from './gh-repo/gh-repo.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GhUsersComponent } from './gh-users/gh-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ {path: '', component: GhUsersComponent }, 
+ {path: 'repo', component: GhRepoComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { RouterModule } from '@angular/router';
+import { UserprofileService } from './userprofile.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GhUsersComponent } from './gh-users/gh-users.component';
+import { GithubService } from './github.service';
+import { GhRepoComponent } from './gh-repo/gh-repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GhUsersComponent,
+    GhRepoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [GithubService, UserprofileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
