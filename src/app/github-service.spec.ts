86e39d32627ea926ccb0f63f336d99760c0e4931
wangilamain <wangilayng@gmail.com>
2020-10-06 12:02:47 -0400

    added new classes

---
+++ src/app/github-service.spec.ts
@@ -0,0 +1,7 @@
+import { GithubService } from './github-service';
+
+describe('GithubService', () => {
+  it('should create an instance', () => {
+    expect(new GithubService()).toBeTruthy();
+  });
+});
